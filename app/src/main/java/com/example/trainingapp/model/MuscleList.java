631c2a2f40cf3c96049ffb8fc71925eec3ffd45f
leoncodev <lionco1>
2022-02-15 12:04:14 +0200

    i dont know why musclelst is save a million times

--- app/src/main/java/com/example/trainingapp/model/MuscleList.java
+++ app/src/main/java/com/example/trainingapp/model/MuscleList.java
@@ -21,54 +21,42 @@ public class MuscleList extends ArrayList<Muscle> {
 
 
     private MuscleList(){
-        add(new Muscle("Pecs"));
-        add(new Muscle("Back"));
-        add(new Muscle("AntDelts"));
-        add(new Muscle("MedDelts"));
-        add(new Muscle("RearDelts"));
-        add(new Muscle("Biceps"));
-        add(new Muscle("Triceps"));
-        add(new Muscle("Quads"));
-        add(new Muscle("Hamstrings"));
-        add(new Muscle("Calves"));
+        this.add(new Muscle("Pecs"));
+        this.add(new Muscle("Back"));
+        this.add(new Muscle("AntDelts"));
+        this.add(new Muscle("MedDelts"));
+        this.add(new Muscle("RearDelts"));
+        this.add(new Muscle("Biceps"));
+        this.add(new Muscle("Triceps"));
+        this.add(new Muscle("Quads"));
+        this.add(new Muscle("Hamstrings"));
+        this.add(new Muscle("Calves"));
     }
-    public void addSets(String[] muscles, int count){
+    public void addSets(String[] muscles, int count) {
         for (String muscle : muscles) {
-            if(muscle.equals("Pecs")){
-                get(0).editCount(count);
-            }else
-            if(muscle.equals("Back")){
-                get(1).editCount(count);
-            }else
-            if(muscle.equals("AntDelts")){
-                get(2).editCount(count);
-            }else
-            if(muscle.equals("MedDelts")){
-                get(3).editCount(count);
-            }else
-            if(muscle.equals("RearDelts")){
-                get(4).editCount(count);
-            }else
-            if(muscle.equals("Biceps")){
-                get(5).editCount(count);
-            }else
-            if(muscle.equals("Triceps")){
-                get(6).editCount(count);
-            }else
-            if(muscle.equals("Quads")){
-                get(7).editCount(count);
-            }else
-            if(muscle.equals("Hamstrings")){
-                get(8).editCount(count);
-            }else
-            if(muscle.equals("Calves")){
-                get(9).editCount(count);
+            if (muscle.equals("Pecs")) {
+                this.get(0).editCount(count);
+            } else if (muscle.equals("Back")) {
+                this.get(1).editCount(count);
+            } else if (muscle.equals("AntDelts")) {
+                this.get(2).editCount(count);
+            } else if (muscle.equals("MedDelts")) {
+                this.get(3).editCount(count);
+            } else if (muscle.equals("RearDelts")) {
+                this.get(4).editCount(count);
+            } else if (muscle.equals("Biceps")) {
+                this.get(5).editCount(count);
+            } else if (muscle.equals("Triceps")) {
+                this.get(6).editCount(count);
+            } else if (muscle.equals("Quads")) {
+                this.get(7).editCount(count);
+            } else if (muscle.equals("Hamstrings")) {
+                this.get(8).editCount(count);
+            } else if (muscle.equals("Calves")) {
+                this.get(9).editCount(count);
             }
 
 
         }
-}
-    public MuscleList getMuscles(){
-        return this;
     }
 }
