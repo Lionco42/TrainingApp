cd2c0eec5bd0cd7343077e20c441411f1d319034
lionco <lionco42@gmail.com>
2022-02-24 10:15:37 +0200

    saving as raw

--- app/src/main/java/com/example/trainingapp/model/MuscleList.java
+++ app/src/main/java/com/example/trainingapp/model/MuscleList.java
@@ -33,7 +33,7 @@ public class MuscleList extends ArrayList<Muscle> implements Serializable {
         if (data.exists()) {
             loadDataFile();
         } else {
-            String str = readDataFromFile(context.getResources().openRawResource(R.raw.muscleList));
+            String str = readDataFromFile(context.getResources().openRawResource(R.raw.muscles));
             Gson gson = new Gson();
             MuscleList moviesArrayList = gson.fromJson(str, MuscleList.class);
             addAll(moviesArrayList);
